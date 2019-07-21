e2e3bdf6816782c2665181e51daef45f4fbc2fba
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-07-21 11:23:13 +0200

    add more algorithms

--- algo.go
+++ algo.go
@@ -1,6 +1,6 @@
 package jwt
 
-// Algorithm represents algorithms for signing and verifying.
+// Algorithm for signing and verifying.
 type Algorithm string
 
 // Algorithm names for signing and verifying.
@@ -8,4 +8,15 @@ const (
 	HS256 Algorithm = "HS256"
 	HS384 Algorithm = "HS384"
 	HS512 Algorithm = "HS512"
+
+	RS256 Algorithm = "RS256"
+	RS384 Algorithm = "RS384"
+	RS512 Algorithm = "RS512"
+
+	ES256 Algorithm = "ES256"
+	ES384 Algorithm = "ES384"
+	ES512 Algorithm = "ES512"
+
+	PS256 Algorithm = "PS256"
+	PS384 Algorithm = "PS384"
 )
