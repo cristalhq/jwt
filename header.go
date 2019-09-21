87adb1ae6b5ad914779ed5d2f1724d1813afc04c
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-09-21 13:54:15 +0200

    extend header

--- header.go
+++ header.go
@@ -1,7 +1,14 @@
 package jwt
 
 // Header stores JWT header data.
+// see https://tools.ietf.org/html/rfc7515
+// and https://tools.ietf.org/html/rfc7519
 type Header struct {
-	Type      string    `json:"typ"`
-	Algorithm Algorithm `json:"alg"`
+	Algorithm      Algorithm `json:"alg"`
+	Type           string    `json:"typ,omitempty"` // type of JWS: it can only be "JWT" here
+	ContentType    string    `json:"cty,omitempty"`
+	JSONKeyURL     string    `json:"jku,omitempty"`
+	KeyID          string    `json:"kid,omitempty"`
+	X509URL        string    `json:"x5u,omitempty"`
+	X509Thumbprint string    `json:"x5t,omitempty"`
 }
