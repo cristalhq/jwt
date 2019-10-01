b5f0f5df3165ba730bd46f9bb435e368eae2f541
Hood Elliot <38839592+HoodElliot@users.noreply.github.com>
2019-10-01 17:27:10 +0300

    Create example_test.go

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package example
+
+import (
+	jwt "github.com/cristalhq/jwt"
+)
+
+func simpleExample() []byte {
+	signer := jwt.NewHS256([]byte(`secret`))
+	builder := jwt.NewTokenBuilder(signer)
+
+	claims := &jwt.StandardClaims{
+		Audience: []string{"admin"},
+		ID:       "random-unique-string",
+	}
+	token, _ := builder.Build(claims)
+
+	return token.Raw() // JWT signed token
+}
