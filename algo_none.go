9df538238c13fcd8ec1ece0bb70ccd1b7c8854a3
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-03 18:56:31 +0200

    add NoSigner

---
+++ algo_none.go
@@ -0,0 +1,20 @@
+package jwt
+
+type noEncryptAlg struct{}
+
+// NewNoEncrypt returns new Signer without encryption. SHOULD NOT BE USED.
+func NewNoEncrypt() Signer {
+	return &noEncryptAlg{}
+}
+
+func (h *noEncryptAlg) Algorithm() Algorithm {
+	return NoEncryption
+}
+
+func (h *noEncryptAlg) Sign(payload []byte) ([]byte, error) {
+	return nil, nil
+}
+
+func (h *noEncryptAlg) Verify(expected, payload []byte) error {
+	return nil
+}
