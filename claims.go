48ebadd86704a7b585b3801fe6a6d5c523309982
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-22 21:08:49 +0200

    add claims checks

--- claims.go
+++ claims.go
@@ -1,6 +1,9 @@
 package jwt
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
 // StandardClaims https://tools.ietf.org/html/rfc7519#section-4.1
 type StandardClaims struct {
@@ -32,6 +35,67 @@ type StandardClaims struct {
 	Subject string `json:"sub,omitempty"`
 }
 
-func (sc *StandardClaims) MarshalBinary() (data []byte, err error) {
+// MarshalBinary default marshaling to JSON.
+func (sc StandardClaims) MarshalBinary() (data []byte, err error) {
 	return json.Marshal(sc)
+}
+
+// IsPermittedFor returns true if claims is allowed to be used by the audience.
+func (sc StandardClaims) IsPermittedFor(audience string) bool {
+	for _, aud := range sc.Audience {
+		if aud == audience {
+			return true
+		}
+	}
+	return false
+}
+
+// IsExpired returns true if the token is expired.
+func (sc StandardClaims) IsExpired(now time.Time) bool {
+	expireAt := sc.ExpiresAt.Time()
+
+	if expireAt.IsZero() {
+		return false
+	}
+	return expireAt.Before(now)
+}
+
+// IsID returns true if claims has the given id.
+func (sc StandardClaims) IsID(id string) bool {
+	return sc.ID == id
+}
+
+// IsIssuedBefore returns true if the token was issued before of given time.
+func (sc StandardClaims) IsIssuedBefore(now time.Time) bool {
+	issuedAt := sc.IssuedAt.Time()
+
+	if issuedAt.IsZero() {
+		return false
+	}
+	return issuedAt.Before(now)
+}
+
+// IsIssuedBy returns true if the token was issued by any of given issuers.
+func (sc StandardClaims) IsIssuedBy(issuers ...string) bool {
+	for _, issuer := range issuers {
+		if sc.Issuer == issuer {
+			return true
+		}
+	}
+	return false
+}
+
+// HasPassedNotBefore returns true if the token activation is used after the given time.
+func (sc StandardClaims) HasPassedNotBefore(now time.Time) bool {
+	notBefore := sc.NotBefore.Time()
+
+	if notBefore.IsZero() {
+		return true
+	}
+	return notBefore.Before(now)
+}
+
+// IsSubject returns true if claims has the given subject.
+func (sc StandardClaims) IsSubject(subject string) bool {
+	return sc.Subject == subject
 }
