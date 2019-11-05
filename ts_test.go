3a3c6824c29f78d5bf092635bd1ca0d14c91dfee
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-11-05 18:57:18 +0100

    improve Timestamp tests

--- ts_test.go
+++ ts_test.go
@@ -3,34 +3,36 @@ package jwt
 import (
 	"encoding/json"
 	"testing"
+	"time"
 )
 
 func TestTimestampMarshal(t *testing.T) {
-	f := func(got Timestamp, want string) {
+	f := func(got Timestamp, want time.Time) {
 		t.Helper()
 
-		raw, err := json.Marshal(got)
-		if err != nil {
-			t.Errorf("want no err, got: %v", err)
-		}
-
-		if string(raw) != want {
-			t.Errorf("want `%v`, got: `%v`", want, string(raw))
+		if got.Time().Equal(want) {
+			t.Errorf("want `%v`, got: `%v`", want, got.Time())
 		}
 	}
 
-	f(0, `0`)
-	f(Timestamp(42), `42`)
+	f(0, time.Time{})
+	f(Timestamp(0), time.Time{})
+
+	now := time.Now()
+	f(Timestamp(now.Unix()), now)
 }
 
 func TestTimestampUnmarshal(t *testing.T) {
-	f := func(got string, want Timestamp) {
+	f := func(got string, want Timestamp, shouldErr bool) {
 		t.Helper()
 
 		var ts Timestamp
 		err := json.Unmarshal([]byte(got), &ts)
-		if err != nil {
+		if err != nil && !shouldErr {
 			t.Errorf("want no err, got: %v", err)
+		}
+		if err == nil && shouldErr {
+			t.Errorf("want err, but got nil")
 		}
 
 		if want != ts {
@@ -38,7 +40,11 @@ func TestTimestampUnmarshal(t *testing.T) {
 		}
 	}
 
-	f(`null`, 0)
-	f(`0`, 0)
-	f(`42`, 42)
+	f(`null`, 0, false)
+	f(`0`, 0, false)
+	f(`42`, 42, false)
+
+	f(``, 0, true)
+	f(`a4`, 0, true)
+	f(`"42"`, 0, true)
 }
