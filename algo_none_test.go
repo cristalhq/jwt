c60a977d8f63f65e5f49a023e5ea960bbe5f141e
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-11-08 19:29:21 +0100

    add NoEncrypy test

---
+++ algo_none_test.go
@@ -0,0 +1,36 @@
+package jwt
+
+import "testing"
+
+func TestNone(t *testing.T) {
+	signer := NewNoEncrypt()
+
+	if signer.Algorithm() != NoEncryption {
+		t.Errorf("want %#v, got #%v", NoEncryption, signer.Algorithm())
+	}
+
+	sign, err := signer.Sign(nil)
+	if err != nil {
+		t.Errorf("want nil, got #%v", err)
+	}
+	if sign != nil {
+		t.Errorf("want nil, got #%v", sign)
+	}
+
+	sign, err = signer.Sign([]byte(`test`))
+	if err != nil {
+		t.Errorf("want nil, got #%v", err)
+	}
+	if sign != nil {
+		t.Errorf("want nil, got #%v", sign)
+	}
+
+	err = signer.Verify(nil, nil)
+	if err != nil {
+		t.Errorf("want nil, got #%v", err)
+	}
+	err = signer.Verify([]byte(`test`), []byte(`test`))
+	if err != nil {
+		t.Errorf("want nil, got #%v", err)
+	}
+}
