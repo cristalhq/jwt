68543dc1365d5bc039012597444e6e2f5f63f0ff
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-05-09 14:32:58 +0200

    Improve parse tests

--- parse_test.go
+++ parse_test.go
@@ -5,17 +5,48 @@ import (
 )
 
 func TestParseString(t *testing.T) {
-	f := func(got string, header Header, payload, signature string) {
+	f := func(token string, header Header, payload, signature string) {
 		t.Helper()
 
-		_, err := ParseString(got)
-		if err == nil {
-			t.Error("got nil want nil")
+		tk, err := ParseString(token)
+		if err != nil {
+			t.Errorf("want nil, got %#v", err)
+		}
+		if tk.Header() != header {
+			t.Errorf("want %#v, got %#v", header, tk.Header())
+		}
+		headerStr := toBase64(headerString(header))
+		if string(tk.RawHeader()) != headerStr {
+			t.Errorf("want %#v, got %#v", headerStr, string(tk.RawHeader()))
+		}
+		if string(tk.Payload()) != payload {
+			t.Errorf("want %#v, got %#v", payload, string(tk.Payload()))
+		}
+		sign := toBase64(string(tk.Signature()))
+		if sign != signature {
+			t.Errorf("want %#v, got %#v", signature, sign)
 		}
 	}
 
-	// TODO
-	f(``, Header{}, "", "")
+	f(
+		`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJqdXN0IGFuIGlkIiwiYXVkIjoiYXVkaWVuY2UifQ.t5oEdZGp0Qbth7lo5fZlV_o4-r9gMoYBSktXbarjWoo`,
+		Header{
+			Algorithm: HS256,
+			Type:      "JWT",
+		},
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJqdXN0IGFuIGlkIiwiYXVkIjoiYXVkaWVuY2UifQ",
+		"t5oEdZGp0Qbth7lo5fZlV_o4-r9gMoYBSktXbarjWoo",
+	)
+	f(
+		`eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCIsImN0eSI6InRva2VuIn0.eyJqdGkiOiJqdXN0IGFuIGlkIiwiYXVkIjoiYXVkaWVuY2UifQ.t5oEdZGp0Qbth7lo5fZlV_o4-r9gMoYBSktXbarjWoo`,
+		Header{
+			Algorithm:   HS512,
+			Type:        "JWT",
+			ContentType: "token",
+		},
+		"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCIsImN0eSI6InRva2VuIn0.eyJqdGkiOiJqdXN0IGFuIGlkIiwiYXVkIjoiYXVkaWVuY2UifQ",
+		"t5oEdZGp0Qbth7lo5fZlV_o4-r9gMoYBSktXbarjWoo",
+	)
 }
 
 func TestParseMalformed(t *testing.T) {
@@ -34,4 +65,9 @@ func TestParseMalformed(t *testing.T) {
 	f(`xyz.abc.x/yz`)
 	f(`x/z.ab_c.xyz`)
 	f(`ab_c.xyz.xyz`)
+}
+
+func headerString(header Header) string {
+	raw, _ := header.MarshalJSON()
+	return string(raw)
 }
