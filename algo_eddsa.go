b5a8f32920e3e530e28147ffc2f0210f5f51d3b3
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-08-19 16:40:04 +0200

    verify EdDSA key on creation step

--- algo_eddsa.go
+++ algo_eddsa.go
@@ -6,7 +6,7 @@ import (
 
 // NewSignerEdDSA returns a new ed25519-based signer.
 func NewSignerEdDSA(key ed25519.PrivateKey) (Signer, error) {
-	if len(key) == 0 {
+	if len(key) == 0 || len(key) != ed25519.PrivateKeySize {
 		return nil, ErrInvalidKey
 	}
 	return &edDSAAlg{
@@ -17,7 +17,7 @@ func NewSignerEdDSA(key ed25519.PrivateKey) (Signer, error) {
 
 // NewVerifierEdDSA returns a new ed25519-based verifier.
 func NewVerifierEdDSA(key ed25519.PublicKey) (Verifier, error) {
-	if len(key) == 0 {
+	if len(key) == 0 || len(key) != ed25519.PublicKeySize {
 		return nil, ErrInvalidKey
 	}
 	return &edDSAAlg{
