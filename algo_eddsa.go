ded0dd6f1d99c126956b3246823e083665d00e65
Oleg Kovalov <oleg@hey.com>
2021-01-14 18:23:42 +0100

    add new Verify method (#90)

--- algo_eddsa.go
+++ algo_eddsa.go
@@ -32,19 +32,23 @@ type edDSAAlg struct {
 	privateKey ed25519.PrivateKey
 }
 
-func (ed edDSAAlg) Algorithm() Algorithm {
+func (ed *edDSAAlg) Algorithm() Algorithm {
 	return ed.alg
 }
 
-func (ed edDSAAlg) SignSize() int {
+func (ed *edDSAAlg) SignSize() int {
 	return ed25519.SignatureSize
 }
 
-func (ed edDSAAlg) Sign(payload []byte) ([]byte, error) {
+func (ed *edDSAAlg) Sign(payload []byte) ([]byte, error) {
 	return ed25519.Sign(ed.privateKey, payload), nil
 }
 
-func (ed edDSAAlg) Verify(payload, signature []byte) error {
+func (ed *edDSAAlg) VerifyToken(token *Token) error {
+	return ed.Verify(token.Payload(), token.Signature())
+}
+
+func (ed *edDSAAlg) Verify(payload, signature []byte) error {
 	if !ed25519.Verify(ed.publicKey, payload, signature) {
 		return ErrInvalidSignature
 	}
