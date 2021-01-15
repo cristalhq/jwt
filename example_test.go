5a84e2c4013f52a7158981af7a66ef65d69cac27
Oleg Kovalov <oleg@hey.com>
2021-01-15 16:17:12 +0100

    update examples (#94)

--- example_test.go
+++ example_test.go
@@ -8,58 +8,74 @@ import (
 	"github.com/cristalhq/jwt/v3"
 )
 
-func Example_JWT() {
-	// 1. create a signer & a verifier
+func Example() {
+	// create a Signer (HMAC in this example)
 	key := []byte(`secret`)
 	signer, err := jwt.NewSignerHS(jwt.HS256, key)
 	checkErr(err)
-	verifier, err := jwt.NewVerifierHS(jwt.HS256, key)
-	checkErr(err)
 
-	// 2. create q standard claims
-	// (you can create your own, see: Example_BuildUserClaims)
-	claims := &jwt.StandardClaims{
+	// create claims (you can create your own, see: Example_BuildUserClaims)
+	claims := &jwt.RegisteredClaims{
 		Audience: []string{"admin"},
 		ID:       "random-unique-string",
 	}
 
-	// 3. create a builder
+	// create a Builder
 	builder := jwt.NewBuilder(signer)
 
-	// 4. and build a token
-	token, errBuild := builder.Build(claims)
-	checkErr(errBuild)
+	// and build a Token
+	newToken, err := builder.Build(claims)
+	checkErr(err)
 
-	// 5. here is your token  :)
-	var _ []byte = token.Raw() // or just token.String() for string
+	// here is token as byte slice
+	var _ []byte = newToken.Raw() // or just token.String() for string
 
-	// 6. parse a token (by example received from a request)
-	tokenStr := token.String()
-	newToken, errParse := jwt.ParseString(tokenStr)
-	checkErr(errParse)
+	// create a Verifier (HMAC in this example)
+	verifier, err := jwt.NewVerifierHS(jwt.HS256, key)
+	checkErr(err)
 
-	// 7. and verify it's signature
-	errVerify := verifier.Verify(newToken.Payload(), newToken.Signature())
-	checkErr(errVerify)
+	// parse a Token (by example received from a request)
+	tokenStr := newToken.String()
+	token, err := jwt.ParseAndVerifyString(tokenStr, verifier)
+	checkErr(err)
 
-	// 8. also you can parse and verify in 1 operation
+	// and verify it's signature
+	err = verifier.Verify(token.Payload(), token.Signature())
+	checkErr(err)
+
+	// also you can parse and verify together
 	newToken, err = jwt.ParseAndVerifyString(tokenStr, verifier)
 	checkErr(err)
 
-	// 9. get standard claims
+	// get standard claims
 	var newClaims jwt.StandardClaims
 	errClaims := json.Unmarshal(newToken.RawClaims(), &newClaims)
 	checkErr(errClaims)
 
-	// 10. verify claims
+	// verify claims as you
 	var _ bool = newClaims.IsForAudience("admin")
 	var _ bool = newClaims.IsValidAt(time.Now())
 
-	fmt.Printf("Algorithm %v\n", newToken.Header().Algorithm)
-	fmt.Printf("Type      %v\n", newToken.Header().Type)
-	fmt.Printf("Claims    %v\n", string(newToken.RawClaims()))
-	fmt.Printf("Payload   %v\n", string(newToken.Payload()))
-	fmt.Printf("Token     %v\n", string(newToken.Raw()))
+	// Output:
+}
+
+func ExampleBuild() {
+	key := []byte(`secret`)
+	signer, _ := jwt.NewSignerHS(jwt.HS256, key)
+	builder := jwt.NewBuilder(signer)
+
+	claims := &jwt.RegisteredClaims{
+		Audience: []string{"admin"},
+		ID:       "random-unique-string",
+	}
+	token, err := builder.Build(claims)
+	checkErr(err)
+
+	fmt.Printf("Algorithm %v\n", token.Header().Algorithm)
+	fmt.Printf("Type      %v\n", token.Header().Type)
+	fmt.Printf("Claims    %v\n", string(token.RawClaims()))
+	fmt.Printf("Payload   %v\n", string(token.Payload()))
+	fmt.Printf("Token     %v\n", string(token.Raw()))
 
 	// Output:
 	// Algorithm HS256
@@ -67,6 +83,81 @@ func Example_JWT() {
 	// Claims    {"jti":"random-unique-string","aud":"admin"}
 	// Payload   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJyYW5kb20tdW5pcXVlLXN0cmluZyIsImF1ZCI6ImFkbWluIn0
 	// Token     eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJyYW5kb20tdW5pcXVlLXN0cmluZyIsImF1ZCI6ImFkbWluIn0.uNaqGEggmy02lZq8FM7KoUKXhOy-zrSF7inYuzIET9o
+}
+
+type userClaims struct {
+	jwt.RegisteredClaims
+	IsAdministrator bool   `json:"is_admin"`
+	Email           string `json:"email"`
+}
+
+func ExampleBuild_WithUserClaims() {
+	key := []byte(`secret`)
+	signer, _ := jwt.NewSignerHS(jwt.HS256, key)
+	builder := jwt.NewBuilder(signer)
+
+	claims := &userClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Audience: []string{"admin"},
+			ID:       "random-unique-string",
+		},
+		IsAdministrator: true,
+		Email:           "foo@bar.baz",
+	}
+	token, err := builder.Build(claims)
+	checkErr(err)
+
+	fmt.Printf("Claims    %v\n", string(token.RawClaims()))
+	fmt.Printf("Payload   %v\n", string(token.Payload()))
+	fmt.Printf("Token     %v\n", string(token.Raw()))
+
+	// Output:
+	// Claims    {"jti":"random-unique-string","aud":"admin","is_admin":true,"email":"foo@bar.baz"}
+	// Payload   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJyYW5kb20tdW5pcXVlLXN0cmluZyIsImF1ZCI6ImFkbWluIiwiaXNfYWRtaW4iOnRydWUsImVtYWlsIjoiZm9vQGJhci5iYXoifQ
+	// Token     eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJyYW5kb20tdW5pcXVlLXN0cmluZyIsImF1ZCI6ImFkbWluIiwiaXNfYWRtaW4iOnRydWUsImVtYWlsIjoiZm9vQGJhci5iYXoifQ.oKE62_k3bqAlKwdBJDBJq5DQ_0FvpNv6e1u6hF_ShQs
+}
+
+func ExampleParse() {
+	rawToken := []byte(`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0.dv9-XpY9P8ypm1uWQwB6eKvq3jeyodLA7brhjsf4JVs`)
+
+	token, err := jwt.Parse(rawToken)
+	checkErr(err)
+
+	fmt.Printf("Algorithm %v\n", token.Header().Algorithm)
+	fmt.Printf("Type      %v\n", token.Header().Type)
+	fmt.Printf("Claims    %v\n", string(token.RawClaims()))
+	fmt.Printf("Payload   %v\n", string(token.Payload()))
+	fmt.Printf("Token     %v\n", string(token.Raw()))
+
+	// Output:
+	// Algorithm HS256
+	// Type      JWT
+	// Claims    {"aud":"admin","jti":"random-unique-string"}
+	// Payload   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0
+	// Token     eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0.dv9-XpY9P8ypm1uWQwB6eKvq3jeyodLA7brhjsf4JVs
+}
+
+func ExampleParseAndVerify() {
+	rawToken := []byte(`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0.dv9-XpY9P8ypm1uWQwB6eKvq3jeyodLA7brhjsf4JVs`)
+
+	key := []byte(`secret`)
+	verifier, _ := jwt.NewVerifierHS(jwt.HS256, key)
+
+	token, err := jwt.ParseAndVerify(rawToken, verifier)
+	checkErr(err)
+
+	fmt.Printf("Algorithm %v\n", token.Header().Algorithm)
+	fmt.Printf("Type      %v\n", token.Header().Type)
+	fmt.Printf("Claims    %v\n", string(token.RawClaims()))
+	fmt.Printf("Payload   %v\n", string(token.Payload()))
+	fmt.Printf("Token     %v\n", string(token.Raw()))
+
+	// Output:
+	// Algorithm HS256
+	// Type      JWT
+	// Claims    {"aud":"admin","jti":"random-unique-string"}
+	// Payload   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0
+	// Token     eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImp0aSI6InJhbmRvbS11bmlxdWUtc3RyaW5nIn0.dv9-XpY9P8ypm1uWQwB6eKvq3jeyodLA7brhjsf4JVs
 }
 
 func checkErr(err error) {
