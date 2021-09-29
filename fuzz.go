95e701e71d22ffc963ba88d5dacf61950bf60b2d
Oleg Kovalov <oleg@hey.com>
2021-09-29 15:41:26 +0200

    Update parsing

--- fuzz.go
+++ fuzz.go
@@ -1,4 +1,6 @@
+//go:build gofuzz
 // +build gofuzz
+
 // To run the fuzzer, run the following commands:
 //		$ GO111MODULE=off go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 //		$ cd $GOPATH/src/github.com/cristalhq/jwt/
@@ -10,7 +12,7 @@
 package jwt
 
 func Fuzz(data []byte) int {
-	if _, err := Parse(data); err != nil {
+	if _, err := ParseNoVerify(data); err != nil {
 		return 0
 	}
 	return 1
