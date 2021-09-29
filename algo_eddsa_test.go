9f519e345a20af9f857c026d8d9d189838ddf402
Oleg Kovalov <oleg@hey.com>
2021-09-29 15:39:35 +0200

    Update EdDSA

--- algo_eddsa_test.go
+++ algo_eddsa_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ed25519"
 	"crypto/rand"
 	"errors"
+	"sync"
 	"testing"
 )
 
@@ -15,42 +16,57 @@ var (
 	ed25519PublicKeyAnother  ed25519.PublicKey
 )
 
-func init() {
-	f := func() (ed25519.PrivateKey, ed25519.PublicKey) {
-		pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
-		if err != nil {
-			panic(err)
-		}
-		return privKey, pubKey
-	}
+var initEdDSAKeysOnce sync.Once
 
-	ed25519PrivateKey, ed25519PublicKey = f()
-	ed25519PrivateKeyAnother, ed25519PublicKeyAnother = f()
+func initEdDSAKeys() {
+	initEdDSAKeysOnce.Do(func() {
+		f := func() (ed25519.PrivateKey, ed25519.PublicKey) {
+			pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
+			if err != nil {
+				panic(err)
+			}
+			return privKey, pubKey
+		}
+
+		ed25519PrivateKey, ed25519PublicKey = f()
+		ed25519PrivateKeyAnother, ed25519PublicKeyAnother = f()
+	})
 }
 
 func TestEdDSA(t *testing.T) {
-	f := func(privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey, isCorrectSign bool) {
+	initEdDSAKeys()
+
+	f := func(privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey, wantErr error) {
 		t.Helper()
 
-		const payload = `simple-string-payload`
-
-		sign := ed25519Sign(t, privateKey, payload)
-
-		err := ed25519Verify(t, publicKey, payload, sign)
-		if err != nil && isCorrectSign {
-			t.Fatal(err)
+		signer, errSigner := NewSignerEdDSA(privateKey)
+		if errSigner != nil {
+			t.Fatalf("NewSignerEdDSA %v", errSigner)
 		}
-		if err == nil && !isCorrectSign {
-			t.Fatal("must be not nil")
+		verifier, errVerifier := NewVerifierEdDSA(publicKey)
+		if errVerifier != nil {
+			t.Fatalf("NewVerifierEdDSA %v", errVerifier)
+		}
+
+		token, err := NewBuilder(signer).Build(simplePayload)
+		if err != nil {
+			t.Fatalf("Build %v", errVerifier)
+		}
+
+		errVerify := verifier.Verify(token)
+		if !errors.Is(errVerify, wantErr) {
+			t.Errorf("want %v, got %v", wantErr, errVerify)
 		}
 	}
 
-	f(ed25519PrivateKey, ed25519PublicKey, true)
-	f(ed25519PrivateKey, ed25519PublicKeyAnother, false)
-	f(ed25519PrivateKeyAnother, ed25519PublicKey, false)
+	f(ed25519PrivateKey, ed25519PublicKey, nil)
+	f(ed25519PrivateKey, ed25519PublicKeyAnother, ErrInvalidSignature)
+	f(ed25519PrivateKeyAnother, ed25519PublicKey, ErrInvalidSignature)
 }
 
 func TestEdDSA_BadKeys(t *testing.T) {
+	initEdDSAKeys()
+
 	f := func(err, wantErr error) {
 		if !errors.Is(err, wantErr) {
 			t.Fatalf("expected %v, got %v", wantErr, err)
@@ -58,35 +74,12 @@ func TestEdDSA_BadKeys(t *testing.T) {
 	}
 
 	f(getSignerError(NewSignerEdDSA(nil)), ErrNilKey)
+
 	priv := ed25519.PrivateKey(make([]byte, 72))
 	f(getSignerError(NewSignerEdDSA(priv)), ErrInvalidKey)
 
 	f(getVerifierError(NewVerifierEdDSA(nil)), ErrNilKey)
+
 	pub := ed25519.PublicKey(make([]byte, 72))
 	f(getVerifierError(NewVerifierEdDSA(pub)), ErrInvalidKey)
-}
-
-func ed25519Sign(t *testing.T, privateKey ed25519.PrivateKey, payload string) []byte {
-	t.Helper()
-
-	signer, errSigner := NewSignerEdDSA(privateKey)
-	if errSigner != nil {
-		t.Fatalf("NewSignerEdDSA %v", errSigner)
-	}
-
-	sign, errSign := signer.Sign([]byte(payload))
-	if errSign != nil {
-		t.Fatalf("SignEdDSA %v", errSign)
-	}
-	return sign
-}
-
-func ed25519Verify(t *testing.T, publicKey ed25519.PublicKey, payload string, sign []byte) error {
-	t.Helper()
-
-	verifier, errVerifier := NewVerifierEdDSA(publicKey)
-	if errVerifier != nil {
-		t.Fatalf("NewVerifierEdDSA %v", errVerifier)
-	}
-	return verifier.Verify([]byte(payload), sign)
 }
