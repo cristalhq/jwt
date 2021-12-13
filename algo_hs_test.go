7f3adf595504e5c139e672f0efb0b7de2f73e3e0
Lucian I. Last <li@last.nl>
2021-12-13 14:07:54 +0100

    Improve error messaging

--- algo_hs_test.go
+++ algo_hs_test.go
@@ -43,13 +43,13 @@ func TestHS(t *testing.T) {
 func TestNewKey(t *testing.T) {
 	key, err := GenerateRandom512Bit()
 	if err != nil {
-		t.Fatalf(" %e", err)
+		t.Fatalf("Error returned directly from GenerateRandom512Bit: %e", err)
 	}
 
 	// 8 bits to 1 byte
 	const byteCount = int(512.0 / 8)
 	if l := len(key); l != byteCount {
-		t.Fatalf("Elength of key is %d, want %d", l, byteCount)
+		t.Fatalf("length of key is %d, want %d", l, byteCount)
 	}
 }
 
